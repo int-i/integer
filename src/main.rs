16bd0d8ada8255a70add37a528543628289b11c6
Seungjae Park <astro.psj@gmail.com>
2021-10-01 15:21:07 +0900

    Add database url figment on rocket config

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ extern crate diesel;
 #[macro_use]
 extern crate rocket;
 
+use rocket::figment::util::map;
+use rocket::Config;
+use std::env;
+use std::fs;
+
 mod db;
 mod models;
 mod routes;
@@ -10,7 +15,24 @@ mod schema;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    let db_name = env::var("DB_NAME").unwrap();
+    let db_user = env::var("DB_USER").unwrap();
+    let db_password_file = env::var("DB_PASSWORD_FILE").unwrap();
+    let db_password = fs::read_to_string(db_password_file)
+        .unwrap()
+        .trim()
+        .to_string();
+
+    let figment = Config::figment().merge((
+        "databases",
+        map! {
+            "integer" => map! {
+                "url" => format!("postgresql://{}:{}@localhost/{}",   db_user, db_password, db_name )
+            }
+        },
+    ));
+
+    rocket::custom(figment)
         .mount("/", routes![routes::index])
         .mount(
             "/members",
