42cb74f1f6362e983c7aa9d9bc8890ff739fa4ab
Seungjae Park <astro.psj@gmail.com>
2021-10-09 09:59:59 +0900

    Refactor db config

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,45 @@
 use rocket_sync_db_pools::database;
 use rocket_sync_db_pools::diesel::PgConnection;
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fs;
+
+#[derive(Deserialize, Serialize)]
+pub struct Config {
+    pub host: String,
+    pub dbname: String,
+    pub user: String,
+    pub password: String,
+    pub uri: String,
+}
+
+impl Config {
+    pub const HOST: &'static str = "localhost";
+    pub const DBNAME: &'static str = "integer";
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        let host = env::var("DB_HOST").unwrap_or_else(|_| Config::HOST.to_string());
+        let dbname = env::var("DB_NAME").unwrap_or_else(|_| Config::DBNAME.to_string());
+        let user = env::var("DB_USER").unwrap();
+        let password = env::var("DB_PASSWORD").unwrap_or_else(|_| {
+            let password_file = env::var("DB_PASSWORD_FILE").unwrap();
+            fs::read_to_string(password_file)
+                .unwrap()
+                .trim()
+                .to_string()
+        });
+        let uri = format!("postgresql://{}:{}@{}/{}", user, password, host, dbname);
+        Self {
+            host,
+            dbname,
+            user,
+            password,
+            uri,
+        }
+    }
+}
 
 #[database("integer")]
 pub struct Conn(PgConnection);
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ extern crate rocket;
 
 use rocket::figment::util::map;
 use rocket::Config;
-use std::env;
-use std::fs;
 
 mod db;
 mod models;
@@ -15,22 +13,9 @@ mod schema;
 
 #[launch]
 fn rocket() -> _ {
-    let db_host = env::var("DB_HOST").unwrap_or_else(|_| "localhost".to_string());
-    let db_name = env::var("DB_NAME").unwrap_or_else(|_| "integer".to_string());
-    let db_user = env::var("DB_USER").unwrap();
-    let db_password_file = env::var("DB_PASSWORD_FILE").unwrap();
-    let db_password = fs::read_to_string(db_password_file)
-        .unwrap()
-        .trim()
-        .to_string();
-    let db_uri = format!(
-        "postgresql://{}:{}@{}/{}",
-        db_user, db_password, db_host, db_name
-    );
-    println!("DB URI - {}", db_uri);
-
-    let figment = Config::figment().merge(("databases", map!["integer" => map!["url" => db_uri]]));
-
+    let db_config = db::Config::default();
+    let figment =
+        Config::figment().merge(("databases", map!["integer" => map!["url" => db_config.uri]]));
     rocket::custom(figment)
         .mount("/", routes![routes::index])
         .mount(
